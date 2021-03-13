c4707743357c1b54514aaccd5a093e095fdbf0d7
Roman Melnikov <Romaneasyrun@yandex.ru>
2021-03-13 13:31:57 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentbalance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int sum = currentbalance + transfer;
+        System.out.println(sum);
+    }
+}
+
